9fa6fc951143ef6f12c838ab0771821238c38fc2
Alexander Shishenko <alex@shishenko.com>
2021-07-27 14:11:17 +0300

    fixup black

--- python_telegram_bot_django_persistence/admin.py
+++ python_telegram_bot_django_persistence/admin.py
@@ -11,22 +11,22 @@ from .models import (
 
 @admin.register(BotData)
 class BotDataAdmin(admin.ModelAdmin):
-    list_display = ("namespace", "data",)
+    list_display = ("namespace", "data")
 
 
 @admin.register(ChatData)
 class ChatDataAdmin(admin.ModelAdmin):
-    list_display = ("namespace", "chat_id",)
+    list_display = ("namespace", "chat_id")
 
 
 @admin.register(UserData)
 class UserDataAdmin(admin.ModelAdmin):
-    list_display = ("namespace", "user_id",)
+    list_display = ("namespace", "user_id")
 
 
 @admin.register(CallbackData)
 class CallbackDataAdmin(admin.ModelAdmin):
-    list_display = ("namespace", "data",)
+    list_display = ("namespace", "data")
 
 
 @admin.register(ConversationData)
