6d2be3f704ad31a15f31d549fe420dd335811730
Alexander Shishenko <alex@shishenko.com>
2024-08-12 05:59:13 +0300

    Update dependencies and persistence module

--- python_telegram_bot_django_persistence/persistence.py
+++ python_telegram_bot_django_persistence/persistence.py
@@ -1,17 +1,16 @@
 import json
-from collections import defaultdict
-from typing import DefaultDict, Optional, Tuple, cast
+from typing import cast
 
 from models import BotData, CallbackData, ChatData, ConversationData, UserData
 from telegram.ext import BasePersistence, PersistenceInput
-from telegram.ext._utils.types import BD, CD, UD, CDCData, ConversationDict
+from telegram.ext._utils.types import BD, CD, UD, CDCData, ConversationDict, ConversationKey
 
 
 class DjangoPersistence(BasePersistence[UD, CD, BD]):
     def __init__(
         self,
         namespace: str = "",
-        store_data: Optional[PersistenceInput] = None,
+        store_data: PersistenceInput | None = None,
         update_interval: float = 60,
     ):
         super().__init__(store_data=store_data, update_interval=update_interval)
@@ -33,11 +32,8 @@ class DjangoPersistence(BasePersistence[UD, CD, BD]):
             for key in orig_keys - set(bot_data.keys()):
                 bot_data.pop(key)
 
-    async def get_chat_data(self) -> DefaultDict[int, CD]:
-        chat_data = {}
-        async for data in ChatData.objects.filter(namespace=self._namespace):
-            chat_data[data.chat_id] = data.data
-        return defaultdict(dict, chat_data)
+    async def get_chat_data(self) -> dict[int, CD]:
+        return {item.chat_id: item.data async for item in ChatData.objects.filter(namespace=self._namespace)}
 
     async def update_chat_data(self, chat_id: int, data: CD) -> None:
         await ChatData.objects.aupdate_or_create(namespace=self._namespace, chat_id=chat_id, defaults={"data": data})
@@ -52,11 +48,8 @@ class DjangoPersistence(BasePersistence[UD, CD, BD]):
         except ChatData.DoesNotExist:
             pass
 
-    async def get_user_data(self) -> DefaultDict[int, UD]:
-        user_data = {}
-        async for data in UserData.objects.filter(namespace=self._namespace):
-            user_data[data.user_id] = data.data
-        return defaultdict(dict, user_data)
+    async def get_user_data(self) -> dict[int, UD]:
+        return {item.user_id: item.data async for item in UserData.objects.filter(namespace=self._namespace)}
 
     async def update_user_data(self, user_id: int, data: UD) -> None:
         await UserData.objects.aupdate_or_create(namespace=self._namespace, user_id=user_id, defaults={"data": data})
@@ -71,7 +64,7 @@ class DjangoPersistence(BasePersistence[UD, CD, BD]):
         except UserData.DoesNotExist:
             pass
 
-    async def get_callback_data(self) -> Optional[CDCData]:
+    async def get_callback_data(self) -> CDCData | None:
         try:
             cdcdata_json = (await CallbackData.objects.aget(namespace=self._namespace)).data
             # Before asking me wtf is this, just check DictPersistence
@@ -83,11 +76,12 @@ class DjangoPersistence(BasePersistence[UD, CD, BD]):
         await CallbackData.objects.aupdate_or_create(namespace=self._namespace, defaults=data)
 
     async def get_conversations(self, name: str) -> ConversationDict:
-        result = {}
-        async for data in ConversationData.objects.filter(namespace=self._namespace, name=name):
-            result[tuple(json.loads(data.key))] = data.state
+        return {
+            tuple(json.loads(data.key)): data.state
+            async for data in ConversationData.objects.filter(namespace=self._namespace, name=name)
+        }
 
-    async def update_conversation(self, name: str, key: Tuple[int, ...], new_state: Optional[object]) -> None:
+    async def update_conversation(self, name: str, key: ConversationKey, new_state: object | None) -> None:
         await ConversationData.objects.aupdate_or_create(
             namespace=self._namespace,
             name=name,
